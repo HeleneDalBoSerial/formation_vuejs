1baefaa6d931d3f5dd087573b756e71be62a0909
Hélène DAL BO <hdalbo@serial.ch>
2020-11-17 18:02:23 +0100

    upload code mardi

---
+++ mardi/1-hello-world/src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" :src="imageUrl" /><br>
+    <input type="number" v-model="nb" v-if="showInput">
+    <p>{{ multiplication }}</p>
+    <button @click="changeInputVisibility()">Change visibility</button>
+    <ul>
+      <li v-for="name in names" :key="name">{{ name }}</li>
+    </ul>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class Home extends Vue {
+  nb = 0;
+  showInput = true;
+  names: string[] = [];
+  imageUrl = "https://media1.giphy.com/media/WV9Xx9itqEIu8GQloO/giphy.gif?cid=ecf05e4775a7936d26aeb4ae8f7b783444bc39ca9d2adda5&rid=giphy.gif"
+
+  created(): void {
+    this.names = ["titi", "tata", "tutu"];
+  }
+
+  get multiplication(): number{
+    return this.nb * 100;
+  }
+
+  changeInputVisibility(){
+    this.showInput = !this.showInput;
+  }
+}
+</script>
---
+++ mardi/3-routing/src/components/Card.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <div class="card">
+      <img :src="item.imgUrl" width="235" height="300"/>
+      <h2>{{ item.name }}</h2>
+      <p>{{ item.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { CardItem } from "@/models/card-item"
+
+@Component
+export default class Card extends Vue {
+  @Prop() private item!: CardItem;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.card {
+  display: flex;
+  flex-direction: column;
+  width: 280px;
+  height: 320px;
+  justify-content: center;
+  background: white;
+  border: 1px solid #ddd;
+  padding: 20px 20px;
+  margin: 10px;
+  h2 {
+    margin: 0;
+    text-align: center;
+  }
+  img {
+    margin: 10px;
+    align-self: center;
+  }
+}
+</style>
