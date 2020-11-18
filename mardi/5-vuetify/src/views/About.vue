b294aae7c4453f882dd908f7862bf51cc81008e6
Hélène DAL BO <hdalbo@serial.ch>
2020-11-18 15:14:00 +0100

    upload fichiers mardi 2/2

--- mardi/5-vuetify/src/views/About.vue
+++ mardi/5-vuetify/src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container>
+    <v-btn block @click="btnClicked">Clique ici</v-btn>
+    <v-card color="#385F73" dark>
+      <v-card-text>
+        <v-list-item>
+          <v-list-item-content v-for="couleur in maListe" :key="couleur">
+            <v-list-item-title>{{ couleur }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+@Component({})
+export default class About extends Vue {
+  maListe = ['Orange', 'Vert', 'Bleu', 'Rouge'];
+
+  btnClicked(): void {
+    alert('Hey!');
+  }
+}
+</script>
